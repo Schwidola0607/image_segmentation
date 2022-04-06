1ffea9981e0b86f8999c048ea6c5cde405a5b19d
brandonfzhang <fireblaze7683@gmail.com>
2022-04-06 01:51:13 +0000

    finished graph constructor

--- graph.cc
+++ graph.cc
@@ -1,1 +1,32 @@
 #include "graph.h"
+#include <cmath>
+
+static long long Graph::BoundaryPenalty(double p, double q){
+    int Ip = static_cast<int>(p * 255), Iq = static_cast<int>(q * 255);
+    return 100 * std::exp(-((Ip - Iq) * (Ip - Iq)) / (2 * SIGMA * SIGMA));
+}
+
+Graph::Graph(string filename){
+    image.readFromFile(filename);
+
+    for(int x=0; x<input.width(); ++x){
+        for(int y=0; y<input.height(); ++y){
+            vertexes.push_back(input.width()*y + x);
+            if(x-1 >= 0){ //left
+                edges.push_back(Edge((input.width()*y + x, input.width()*y + x-1, BoundaryPenalty(image.getPixel(x,y).l, image.getPixel(x-1,y).l))));
+            }
+            if(x+1 < input.width()){ //right
+                edges.push_back(Edge((input.width()*y + x, input.width()*y + x+1, BoundaryPenalty(image.getPixel(x,y).l, image.getPixel(x+1,y).l))));
+            }
+            if(y-1 >= 0){ //up
+                edges.push_back(Edge((input.width()*y + x, input.width()*(y-1) + x, BoundaryPenalty(image.getPixel(x,y).l, image.getPixel(x,y-1).l))));
+            }
+            if(y+1 < input.height()){ //down
+                edges.push_back(Edge((input.width()*y + x, input.width()*(y+1) + x, BoundaryPenalty(image.getPixel(x,y).l, image.getPixel(x,y+1).l))));
+            }
+        }
+    }
+    //first one is the source, second one is the sink
+    vertexes.push_back(input.width() * input.height());
+    vertexes.push_back(input.width() * input.height() + 1);
+}
--- graph.h
+++ graph.h
@@ -3,13 +3,25 @@
 #include <vector>
 #include <string>
 #include "edge.h"
+#include "cs225/PNG.h"
 
 using std::vector;
 using std::string;
 
 class Graph {
+  PNG image;
   vector<Edge> edges; 
   vector<int> vertexes;
 
-  Graph(string filename);
+  //takes in two luminance values from 0.0-1.0
+  static long long BoundaryPenalty(double p, double q);
+  static constexpr unsigned SIGMA = 30;
+
+  public:
+    //initializes graph with n-links
+    Graph(string filename);
+
+    //add t-link methods
+    AddBSeed(unsigned x, unsigned y);
+    AddFSeed(unsigned x, unsigned y);
 };
