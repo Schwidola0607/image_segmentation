0f3f81622942f0171efe5fc2de00a46e151210e9
Zhang, Brandon <bz16@illinois.edu>
2022-04-07 23:08:44 -0500

    made edge comparisons const

--- edge.h
+++ edge.h
@@ -8,10 +8,10 @@ struct Edge {
   Edge(int from, int to, long long weight): from(from), to(to), weight(weight){}
 
   //comparison operators - compares weights only
-  bool operator>(const Edge& other_edge) {return weight > other_edge.weight;}
-  bool operator==(const Edge& other_edge) {return weight == other_edge.weight;}
-  bool operator!=(const Edge& other_edge) {return !(*this == other_edge);}
-  bool operator<(const Edge& other_edge) {return weight < other_edge.weight;}
-  bool operator>=(const Edge& other_edge) {return *this > other_edge || *this == other_edge;}
-  bool operator<=(const Edge& other_edge) {return *this < other_edge || *this == other_edge;}
+  bool operator>(const Edge& other_edge) const {return weight > other_edge.weight;}
+  bool operator==(const Edge& other_edge) const {return weight == other_edge.weight;}
+  bool operator!=(const Edge& other_edge) const {return !(*this == other_edge);}
+  bool operator<(const Edge& other_edge) const {return weight < other_edge.weight;}
+  bool operator>=(const Edge& other_edge) const {return *this > other_edge || *this == other_edge;}
+  bool operator<=(const Edge& other_edge) const {return *this < other_edge || *this == other_edge;}
 };
