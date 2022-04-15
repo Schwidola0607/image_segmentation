45d42d0ee10059def1b58d4e39be682a33c74659
khoadp2 <khoadp2@illinois.edu>
2022-04-15 20:22:51 +0000

    implement Edmond-Karps + changes to graph and edge class

---
+++ edmondkarps.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <vector>
+#include <algorithm>
+#include "graph.h"
+#include <queue>
+
+using std::vector;
+using std::queue;
+using std::pair;
+using std::min; 
+
+const long long INF = INT64_MAX;
+
+class EdmondKarps {
+  private:
+    Graph* g;
+  public: 
+    long long bfs(int s, int t, vector <int>& parent);
+    long long maxFlow();
+    vector <pair<int, int>> getBackground();
+};
