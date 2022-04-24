1d7db1c97086325b99b9f7796a8afae25d6b44a4
brandonfzhang <fireblaze7683@gmail.com>
2022-04-24 05:14:49 +0000

    reorganized 3x3 into tests folder

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -19,61 +19,61 @@ PNG drawEdmond(Graph& g){
 }
 
 TEST_CASE("Edmond_Karps 3x3 #1"){
-    Graph g("3x3tests/1.png");
+    Graph g("tests/3x3tests/1.png");
     g.AddFSeed(2,1);
     g.AddBSeed(1,1);
     g.AddBSeed(0,0);
     
     PNG expected;
-    expected.readFromFile("3x3tests/1_expected.png");
+    expected.readFromFile("tests/3x3tests/1_expected.png");
     PNG actual = drawEdmond(g);
-    actual.writeToFile("tests/1_actual.png");
+    actual.writeToFile("tests/3x3tests/1_actual.png");
     REQUIRE(expected == actual);
 }
 
 TEST_CASE("Edmond_Karps 3x3 #2"){
-    Graph g("3x3tests/2.png");
+    Graph g("tests/3x3tests/2.png");
     g.AddFSeed(1,0);
     g.AddFSeed(1,1);
     g.AddBSeed(1,2);
     
     PNG expected;
-    expected.readFromFile("3x3tests/2_expected.png");
+    expected.readFromFile("tests/3x3tests/2_expected.png");
     PNG actual = drawEdmond(g);
-    actual.writeToFile("tests/2_actual.png");
+    actual.writeToFile("tests/3x3tests/2_actual.png");
     REQUIRE(expected == actual);
 }
 
 TEST_CASE("Edmond_Karps 3x3 #3"){
-    Graph g("3x3tests/3.png");
+    Graph g("tests/3x3tests/3.png");
     g.AddFSeed(1,1);
     g.AddFSeed(1,2);
     g.AddBSeed(2,0);
     g.AddBSeed(2,1);
     
     PNG expected;
-    expected.readFromFile("3x3tests/3_expected.png");
+    expected.readFromFile("tests/3x3tests/3_expected.png");
     PNG actual = drawEdmond(g);
-    actual.writeToFile("tests/3_actual.png");
+    actual.writeToFile("tests/3x3tests/3_actual.png");
     REQUIRE(expected == actual);
 }
 
 TEST_CASE("Edmond_Karps 3x3 #4"){
-    Graph g("3x3tests/4.png");
+    Graph g("tests/3x3tests/4.png");
     g.AddFSeed(0,1);
     g.AddFSeed(2,1);
     g.AddFSeed(1,0);
     g.AddBSeed(1,2);
     
     PNG expected;
-    expected.readFromFile("3x3tests/4_expected.png");
+    expected.readFromFile("tests/3x3tests/4_expected.png");
     PNG actual = drawEdmond(g);
-    actual.writeToFile("tests/4_actual.png");
+    actual.writeToFile("tests/3x3tests/4_actual.png");
     REQUIRE(expected == actual);
 }
 
 TEST_CASE("Edmond_Karps 3x3 #5"){
-    Graph g("3x3tests/5.png");
+    Graph g("tests/3x3tests/5.png");
     g.AddFSeed(1,2);
     g.AddBSeed(2,0);
     g.AddBSeed(2,2);
@@ -81,8 +81,8 @@ TEST_CASE("Edmond_Karps 3x3 #5"){
     g.AddBSeed(1,1);
     
     PNG expected;
-    expected.readFromFile("3x3tests/5_expected.png");
+    expected.readFromFile("tests/3x3tests/5_expected.png");
     PNG actual = drawEdmond(g);
-    actual.writeToFile("tests/5_actual.png");
+    actual.writeToFile("tests/3x3tests/5_actual.png");
     REQUIRE(expected == actual);
 }
