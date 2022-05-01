1e15660d60b5077c15f1f3d017df8e32c280d503
brandonfzhang <fireblaze7683@gmail.com>
2022-05-01 19:34:59 +0000

    removed unnecessary edge operators

--- edge.h
+++ edge.h
@@ -6,12 +6,4 @@ struct Edge {
   long long cap, flow = 0;
 
   Edge(int from, int to, long long cap): from(from), to(to), cap(cap){}
-
-  //comparison operators - compares weights only
-  bool operator>(const Edge& other_edge) const {return cap > other_edge.cap;}
-  bool operator==(const Edge& other_edge) const {return cap == other_edge.cap;}
-  bool operator!=(const Edge& other_edge) const {return !(*this == other_edge);}
-  bool operator<(const Edge& other_edge) const {return cap < other_edge.cap;}
-  bool operator>=(const Edge& other_edge) const {return *this > other_edge || *this == other_edge;}
-  bool operator<=(const Edge& other_edge) const {return *this < other_edge || *this == other_edge;}
 };
